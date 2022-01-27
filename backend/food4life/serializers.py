161243437606e1a915b761caf0cdc9da3cba94fc
Andrii <sparrow.andriy@gmail.com>
2022-01-27 15:58:44 +0200

    added serializers

---
+++ backend/food4life/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import User, Recipe, Product, Category, Ingredient, Rating
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'fname', 'lname', 'email', 'password')
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = ('id', 'difficulty', 'description', 'name', 'type', 'img_path', 'est_time')
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'img_path', 'price', 'proteins', 'fats', 'carbs', 'measure_type')
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'alias', 'product_id')
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'amount', 'product_id', 'recipe_id')
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ('id', 'rating', 'recipe_id', 'user_id')
