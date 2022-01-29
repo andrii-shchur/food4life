9f33f4a84e3a26e1630507bce4cf28994d0e8a64
Andrii <sparrow.andriy@gmail.com>
2022-01-29 17:46:11 +0200

    added two test methods

--- backend/food4life/serializers.py
+++ backend/food4life/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User, Recipe, Product, Category, Ingredient, Rating
+from .models import *
 
 
 class UserSerializer(serializers.ModelSerializer):
